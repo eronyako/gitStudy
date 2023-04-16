2ae65313d5b60aebbc12c593d3f02d3365b04e81
eronyako <1138345264@qq.com>
2023-04-16 11:14:17 +0800

    显示当前参数

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ import platform
 if __name__ == '__main__':
     print(f"Python {sys.version}")
     print(f"{platform.node()}: {platform.system()}, {platform.architecture()}")
+    print(f"argv: {sys.argv}")
 
